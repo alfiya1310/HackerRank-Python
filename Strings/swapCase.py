8f5cfb8f2154d2e732b2b321b98aed5f0ae8064d
alfiya1310 <74983960+alfiya1310@users.noreply.github.com>
2022-10-06 21:48:33 +0530

    resolved merge conflict

--- Strings/swapCase.py
+++ Strings/swapCase.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 def swap_case(s):
     return s.swapcase()
 
 if __name__ == '__main__':
     s = input()
     result = swap_case(s)
-=======
-def swap_case(s):
-    return s.swapcase()
-
-if __name__ == '__main__':
-    s = input()
-    result = swap_case(s)
->>>>>>> 8776f455b5a1fecc4ede233c2c34700136979f40
     print(result)
