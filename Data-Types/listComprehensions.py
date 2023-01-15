bf9863ad46822df39f1fc20a4183b4d8cdf9b5bd
alfiya1310 <74983960+alfiya1310@users.noreply.github.com>
2023-01-15 23:21:38 +0530

    edited stuff

--- Data-Types/listComprehensions.py
+++ Data-Types/listComprehensions.py
@@ -4,10 +4,10 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    l=[]
-    for a in range(x+1):
-        for b in range(y+1):
-            for c in range(z+1):
-                if a+b+c!=n:
-                    l.append([a,b,c])
+    l = []
+    for a in range(x + 1):
+        for b in range(y + 1):
+            for c in range(z + 1):
+                if a + b + c != n:
+                    l.append([a, b, c])
     print(l)
